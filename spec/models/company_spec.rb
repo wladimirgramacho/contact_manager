b3a6f19d9b9afec7ab20efe0445c1abd530b5624
Wladimir Gramacho <wladimirgramacho@gmail.com>
2016-04-13 21:11:13 -0300

    Company is valid

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:company) { Company.new(name:'Apple')}
+
+	it 'is valid' do
+		expect(company).to be_valid
+	end
+
+	it 'is invalid' do
+		company.name = nil
+		expect(company).to_not be_valid
+	end
+
 end
