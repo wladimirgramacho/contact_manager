77321673410583311f00ce044591a337b582efa4
Wladimir Gramacho <wladimirgramacho@gmail.com>
2016-04-13 21:04:31 -0300

    Company Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404224355) do
+ActiveRecord::Schema.define(version: 20160414000240) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "email_addresses", force: :cascade do |t|
     t.string   "email"
