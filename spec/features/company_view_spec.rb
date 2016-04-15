f2774b21926935b6b8246c187a0950697fc91bf5
Wladimir Gramacho <wladimirgramacho@gmail.com>
2016-04-14 21:17:14 -0300

    Company show has phone numbers (edit & delete)

---
+++ spec/features/company_view_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+describe 'the company view', type: :feature do
+	let(:company) {Company.create(name: 'Apple')}
+
+	describe 'phone numbers' do #phone numbers
+
+		before(:each) do
+			company.phone_numbers.create(number: '555-1234')
+			company.phone_numbers.create(number: '555-1334')
+			visit company_path(company)
+		end
+
+		it 'shows the phone numbers' do
+			company.phone_numbers.each do |phone|
+				expect(page). to have_content(phone.number)
+			end
+		end
+
+		it 'has a link to add a phone number' do
+			expect(page).to have_link('Add phone number', href: new_phone_number_path(contact_id: company.id, contact_type: 'Company'))
+		end
+
+		it 'has links to edit phone number' do
+			company.phone_numbers.each do |phone|
+				expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+			end
+		end
+
+		it 'has links to delete phone number' do
+			company.phone_numbers.each do |phone|
+			expect(page).to have_link('delete', href: phone_number_path(phone))
+			end
+		end
+
+		it 'ensures phone number is destroyed entirely' do
+			phone = company.phone_numbers.first
+
+			first(:link, 'delete').click
+			expect(current_path).to eq(company_path(company))
+			expect(page).to_not have_content(phone)
+		end
+
+		it 'edits a phone number' do
+			phone = company.phone_numbers.first
+			old_number = phone.number
+
+			first(:link, 'edit').click
+			page.fill_in('Number', with: '555-2234')
+			page.click_button('Update Phone number')
+			expect(current_path).to eq(company_path(company))
+			expect(page).to have_content('555-2234')
+			expect(page).to_not have_content(old_number)
+		end
+		
+		it 'adds a phone number and return to contact show' do
+			page.click_link('Add phone number')
+			page.fill_in('Number', with: '555-8888')
+			page.click_button('Create Phone number')
+			expect(current_path).to eq(company_path(company))
+			expect(page).to have_content('555-8888')
+		end
+	end
+end
